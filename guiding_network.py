fixed normalization in guiding system

guiding_network.py
@@ -47,8 +47,8 @@ class GuidingNetwork(nn.Module):
                 "otype": "FullyFusedMLP",
                 "activation": "ReLU",
                 "output_activation": "None", # IMPORTANT: Activations are applied manually
-                "n_neurons": 128,
-                "n_hidden_layers": 8,
+                "n_neurons": 64,
+                "n_hidden_layers": 4,
             }
         }
 
@@ -110,11 +110,10 @@ class GuidingNetwork(nn.Module):
         """
         # Set to evaluation mode and disable gradients for inference speed
         self.model.eval()
+        
         with torch.no_grad():
             input_tensor = self._prepare_input(positions, view_dirs, roughness)
             # The network expects inputs in the [0, 1] range.
-            # Normalizing with the scene AABB is the proper way to do this.
-            # A simple (pos + 1)/2 is a placeholder for a [-1, 1] world.
             raw_output = self.model(input_tensor.to(self.device))
         
         # Process the raw output to get meaningful parameters

path_guiding_system.py
@@ -8,7 +8,7 @@ import drjit as dr
 from math_utils import *
 
 class PathGuidingSystem:
-    def __init__(self, device: str = "cuda", K: int = 8, learning_rate: float = 1e-2):
+    def __init__(self, device: str = "cuda", K: int = 20, learning_rate: float = 1e-2):
         self.device = device
         self.gnn = GuidingNetwork(device, K=K).to(device)
         self.optimizer = torch.optim.AdamW(self.gnn.model.parameters(), lr=learning_rate)
@@ -22,9 +22,6 @@ class PathGuidingSystem:
         rec = integrator.surfaceInteractionRecord
         integrator.scatter_data_into_buffer()
 
-        self.bbox_max = integrator.bbox_max.torch().to("cuda")
-        self.bbox_min = integrator.bbox_min.torch().to("cuda")
-
         if dr.width(rec.position) == 0:
             return (None,) * 6
 
@@ -32,7 +29,10 @@ class PathGuidingSystem:
         wi = rec.wi.torch() # Direction of incoming ray
         wo = rec.wo.torch() # direction sampled using bsdf
 
-        pos = (pos - self.bbox_min)/(self.bbox_max - self.bbox_min)
+        pos = 2 * ((pos - self.bbox_min)/(self.bbox_max - self.bbox_min)) - 1
+        
+        # print(pos.min(), pos.max())
+        # print(self.bbox_min, self.bbox_max)
 
         targets_li = rec.radiance.torch()
         combined_pdf = rec.woPdf.torch()
@@ -141,8 +141,10 @@ class PathGuidingSystem:
 
     def pdf(self, position, wi, roughness, wo):
         self.gnn.eval()
+
         with torch.no_grad():
-            position = (position - self.bbox_min)/(self.bbox_max - self.bbox_min)
+            position = 2 * ((position - self.bbox_min)/(self.bbox_max - self.bbox_min))  - 1
+
             vmf_params = self.gnn(position, wi, roughness)
             guiding_dist = BatchedMixedSphericalGaussianDistribution(vmf_params)
             pdf_val = guiding_dist.pdf(wo)
@@ -154,8 +156,10 @@ class PathGuidingSystem:
         Uses the trained network to sample a new direction.
         """
         self.gnn.eval()
+
         with torch.no_grad():
-            position = (position - self.bbox_min)/(self.bbox_max - self.bbox_min)
+            position = 2 * ((position - self.bbox_min)/(self.bbox_max - self.bbox_min)) - 1
+
             vmf_params = self.gnn.query(position, wi, roughness)
             
             guiding_dist = BatchedMixedSphericalGaussianDistribution(vmf_params)